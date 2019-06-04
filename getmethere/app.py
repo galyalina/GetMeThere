05ecb133256c99838983b837574f41cc9614a5ba
Galya Litkin <glitkin@gmail.com>
2019-06-04 12:50:25 +0200

    Fist commit

---
+++ getmethere/app.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  4 12:31:20 2019
+
+@author: Galya
+"""
+
+def main():
+  print("Hello World!")
+  
+if __name__== "__main__":
+  main()
+
+print("Main function launched")
---
+++ getmethere/common/common.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  4 12:56:45 2019
+
+@author: Galya
+"""
+
